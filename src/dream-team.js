055bac216625056e443b43ebb1dcd5ab495dc817
AlexGorSer <taskekaminari@gmail.com>
2022-10-16 15:50:28 +0200

    feat: add 3 solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+
+  
   // remove line with error and write your code here
+
+  function sortArr(members) {
+  for (let i = 0; i < members.length; i++){
+      if (typeof members[i] === "string"){
+          members[i] = members[i].replace(/\s+/g,'');
+      }   
+  }
+};
+
+function createDreamTeam(members) {
+if (Array.isArray(members)){
+sortArr(members);
+let teamName = '';
+for (let i = 0; i < members.length; i++) {
+    if(typeof members[i] === "string") {
+        let first = members[i].charAt(0);
+        teamName = teamName + first;
+    }
+    teamName = teamName.toUpperCase();
+    teamName = teamName.split('').sort().join('');
+} return teamName;
+}return false;
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -11,8 +11,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
+function getSeason(date) {
+  if(arguments.length === 0) {
+    return 'Unable to determine the time of year!';
+  };
+  if(!(date instanceof Date)) {
+    throw new Error('Invalid date!');
+  }
+  if (Object.getOwnPropertySymbols(date).length > 0) {
+    throw new Error("Invalid date!");
+  }
+  let mount = date.getMonth() + 1;
+  if(mount === 1 || mount === 2 || mount === 12){
+    return 'winter';
+  }
+  else if(mount === 3 || mount === 4 || mount === 5){
+    return 'spring';
+  }
+  else if(mount === 6 || mount === 7 || mount === 8){
+    return 'summer';
+  }
+  else if(mount === 9 || mount === 10 || mount === 11){
+    return 'autumn';
+  }
   // remove line with error and write your code here
 }
 
